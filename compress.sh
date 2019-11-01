7775af0468d6d03c93bfca5f9a2d7616d7a1b1c2
michele <capimichi@gmail.com>
2019-11-01 22:05:18 +0100

    Sh 1

--- compress.sh
+++ compress.sh
@@ -3,7 +3,8 @@
 cat directories.txt | while read f
 do
     echo "$f"
-    mkdir -p "./build/${f}"
-    tar -zcvf "./build/${f}/master.tar.gz" "./src/${f}"
-    zip -r "./build/${f}/master.zip" "./src/${f}"
+    mkdir -p "../build/${f}"
+    cd "src"
+    tar -zcvf "../build/${f}/master.tar.gz" "./${f}"
+    zip -r "../build/${f}/master.zip" "./${f}"
 done
